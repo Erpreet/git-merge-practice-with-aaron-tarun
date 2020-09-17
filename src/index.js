82573e37c130a4e26aa8340a14e0a66041f33443
Aaron Barthel <aaron.barthel@live.ca>
2020-09-17 14:36:21 -0600

    AaronComponent is awesome!

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import AaronsComponent from './components/AaronsComponent'
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <AaronsComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
