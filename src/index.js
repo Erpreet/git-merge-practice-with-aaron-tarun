db8cb26448b2e737efd6d9fb0002b4fe44864d24
Erpreet <preetkaurchd15@gmail.com>
2020-09-17 14:54:33 -0600

    fixes done to HelloPreet

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloTarun from './components/HelloTarun';
 import './index.css';
-import AaronsComponent from './components/AaronsComponent'
+import AaronsComponent from './components/AaronsComponent';
+import HelloPreet from './components/HelloPreet';
 
 ReactDOM.render(
   <React.StrictMode>
     <AaronsComponent />
     <HelloTarun />
+    <HelloPreet />
   </React.StrictMode>,
   document.getElementById('root')
 );
